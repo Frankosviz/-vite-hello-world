d4577bc87473bb0e87889fe82808b371d77539d2
Frankosviz <geom.francescopaolo@gmail.com>
2024-04-09 23:03:38 +0200

    vite-hello-world : coding

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,17 @@
 </template>
 
 <script>
-import AppTitle from './components/AppTitle.vue'
+    import AppTitle from './components/AppTitle.vue'
     export default {
+        name: 'App',
+
+        components: {
+            AppTitle
+        },
+        data(){
+            return {
+            }
+        },
 
         
     }
--- src/components/AppTitle.vue
+++ src/components/AppTitle.vue
@@ -1,19 +1,32 @@
 <template>
-    
+        <!-- Nel template inserisco il mio titolo in un div container -->
+    <div class="container">
+        <h1>{{ title }}</h1>
+    </div>
 </template>
 
 <script>
-    export default {
-        name : 'AppTitle',
-        data(){
-            return {
-                title : 'La mia prima App con vite!'
-            }
+export default {
+        // Nel mio script inizio dando un nome a questo componente, mi servirà nella App.vue per richiamarlo nel template
+    name: 'AppTitle',
+    data() {
+        // Nei data gli faccio ritornare una stringa di testo
+        return {
+            title: 'La mia prima App con vite!'
         }
-        
     }
+
+}
 </script>
 
 <style scoped>
-
+/* Aggiungo qualche stile css al mio componente */
+.container {
+    background-color: black;
+}
+h1 {
+    color: red;
+    text-align: center;
+    font-size: 50px;
+}
 </style>
